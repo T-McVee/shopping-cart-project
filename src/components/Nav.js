6d51e3f8bd75efb7412e34bb5b781e170aab1a9a
T-McVee <iam@tmcvee.com>
2021-04-01 08:38:48 -0700

    add fa icons to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 export const Nav = () => {
   return (
@@ -10,12 +13,21 @@ export const Nav = () => {
         </Link>
       </div>
       <ul className="nav-links">
-        <Link to="/shop" className="link">
-          <li>shop</li>
-        </Link>
-        <Link to="/cart" className="link">
-          <li>cart</li>
-        </Link>
+        <li>
+          <Link to="/shop" className="link">
+          shop
+          </Link>  
+        </li>
+        <li>
+          <Link to="/cart" className="link">
+            <FontAwesomeIcon icon={faShoppingCart} />
+          </Link>
+        </li>
+        <li className="link">
+          <a className="link" href="https://github.com/T-McVee/shopping-cart-project" target="blank">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </li>
       </ul>
     </nav>
   )
