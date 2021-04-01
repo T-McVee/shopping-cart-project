591323e81e99d4ad2b46d37e202a4e8a039cb563
T-McVee <iam@tmcvee.com>
2021-03-31 20:56:03 -0700

    Add react router & dart sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './css/style.css';
+import { Nav } from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h3>Logo</h3>
+      </div>
+      <ul className="nav-links">
+        <li className="link">Link</li>
+        <li className="link">Link</li>
+        <li className="link">Link</li>
+      </ul>
+    </nav>
+  )
+}
