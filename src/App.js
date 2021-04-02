7f0422f373f90f3331b1f3da63045dfbcb659ebd
T-McVee <iam@tmcvee.com>
2021-04-02 11:46:27 -0700

    add product filtering

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
     const data = await fetch('https://fakestoreapi.com/products');
     const products = await data.json();
     setProducts(products);
+    //console.log(products);
   }
 
   const addToCart = (product, qty) => {
--- src/Shop.js
+++ src/Shop.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import uuid from 'react-uuid';
-import {Link} from 'react-router-dom';
 import { ShopProductCard } from './components/ShopProductCard';
 
 export const Shop = props => {
   const {products, addToCart} = props;
   
+  const [category, setCategory] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState(products);
+
+  const filterProducts = (x) => {
+    // only display products from selected category
+    
+      const filteredProducts = products.filter(prod => prod.category === x)
+      setFilteredProducts(filteredProducts);
+    
+
+    return '';
+  }
+
   return (
     <div id="shop" className="page page-shop">
       <h1>Buy things</h1>
       <section className="categories">
-        <div className="category">Men's Clothing</div>
-        <div className="category">Women's Clothing</div>
-        <div className="category">Jewlery</div>
-        <div className="category">Electronics</div>
+        <div className="category" onClick={() => filterProducts('men clothing')}>Men's Clothing</div>
+        <div className="category" onClick={() => filterProducts('women clothing')}>Women's Clothing</div>
+        <div className="category" onClick={() => filterProducts('jewelery')}>Jewlery</div>
+        <div className="category" onClick={() => filterProducts('electronics')}>Electronics</div>
       </section>
       <section className="product-feed">
-        {products.map(prod => <ShopProductCard product={prod} key={uuid()} addToCart={addToCart}/>)}
+        {filteredProducts.map(prod => <ShopProductCard product={prod} key={uuid()} addToCart={addToCart}/>)}
       </section>
     </div>
   )
