f8baf9e93c738a21711dce0448d98cf9b5097f87
T-McVee <iam@tmcvee.com>
2021-04-05 07:40:35 -0700

    update categories list to ul & add 'All' category

--- src/Shop.js
+++ src/Shop.js
@@ -20,11 +20,13 @@ export const Shop = props => {
     <div id="shop" className="page page-shop">
       <h1>Buy things</h1>
       <section className="categories">
-        <div className="category" onClick={() => resetFilter()}>All</div>
-        <div className="category" onClick={() => filterProducts('men clothing')}>Men's Clothing</div>
-        <div className="category" onClick={() => filterProducts('women clothing')}>Women's Clothing</div>
-        <div className="category" onClick={() => filterProducts('jewelery')}>Jewlery</div>
-        <div className="category" onClick={() => filterProducts('electronics')}>Electronics</div>
+        <ul className="categories-list">
+          <li className="category" onClick={() => resetFilter()}>All</li>
+          <li className="category" onClick={() => filterProducts('men clothing')}>Men's Clothing</li>
+          <li className="category" onClick={() => filterProducts('women clothing')}>Women's Clothing</li>
+          <li className="category" onClick={() => filterProducts('jewelery')}>Jewlery</li>
+          <li className="category" onClick={() => filterProducts('electronics')}>Electronics</li>
+        </ul>
       </section>
       <section className="product-feed">
         {filteredProducts.map(prod => (
