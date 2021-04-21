f7fae98bbf8294f84652977ca1ad480a820f8f15
T-McVee <iam@tmcvee.com>
2021-04-20 20:49:23 -0700

    remove redundat github icon from home screen

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-
 
 export const Home = () => {
   return (
